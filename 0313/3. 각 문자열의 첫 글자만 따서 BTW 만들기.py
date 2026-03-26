9d9d79da2813c43842f395ffbb34de7db7ed66c9
killer46889 <killer46889@gmail.com>
2026-03-26 16:43:08 +0900

    Add files via upload

---
+++ 0313/3. 각 문자열의 첫 글자만 따서 BTW 만들기.py
@@ -0,0 +1,8 @@
+3. 각 문자열의 첫 글자만 따서 "BTW" 만들기
+
+a = "By"
+b = "The"
+c = "Way"
+
+result = a[0] + b[0] + c[0]
+print(result)
---
+++ 0313/밑변이10이고높이가10인삼각형의면적을계산하는프로그램.py
@@ -0,0 +1,8 @@
+밑변이10이고높이가10인삼각형의면적을계산하는프로그램
+
+base = 10
+height = 10
+
+area = base * height / 2
+
+print("삼각형의 면적:", area)
